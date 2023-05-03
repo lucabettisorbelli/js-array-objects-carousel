b09b85b5f8aff47cfc2ea1f8bf43075dcae5ed7f
luca <Luca betti sorbelli>
2023-05-03 16:59:11 +0200

    lavro finito su base dello scorso lavoro

--- js/script.js
+++ js/script.js
@@ -21,3 +21,118 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+let box1 = document.querySelector(".box1");
+let box2 = document.querySelector(".box2");
+let thumbBox = document.createElement("div");
+thumbBox.classList.add("thumbBox");
+box2.appendChild(thumbBox);
+
+for (let i = 0; i < images.length; i++) {
+    // Crea un div per contenere l'immagine, il titolo e il testo
+    let slide = document.createElement("div");
+    slide.classList.add("slide");
+    // Nascondi tutte le slide tranne la prima
+    if (i > 0) {
+        slide.classList.add("hidden");
+    }
+    box1.appendChild(slide);
+
+    // Crea l'immagine
+    let img = document.createElement("img");
+    img.src = `./${images[i].image}`;
+    slide.appendChild(img);
+
+    // Crea il titolo
+    let title = document.createElement("h2");
+    title.textContent = images[i].title;
+    slide.appendChild(title);
+
+    // Crea il testo
+    let text = document.createElement("p");
+    text.textContent = images[i].text;
+    slide.appendChild(text);
+
+    // Crea la miniatura
+    let thumb = document.createElement("img");
+    thumb.src = images[i].image;
+    thumb.classList.add("thumb");
+    // Attiva l'opacità e il bordo per la prima miniatura
+    if (i == 0) {
+        thumb.classList.add("activeThumb");
+    }
+    thumbBox.appendChild(thumb);
+}
+
+
+let slides = document.getElementsByClassName("slide");
+let thumbs = document.getElementsByClassName("thumb");
+let btnBack = document.querySelector(".btn-primary_1");
+let btnNext = document.querySelector(".btn-primary_2");
+let immagineCorrente = 0;
+
+btnBack.addEventListener("click", function () {
+    if (immagineCorrente > 0) {
+        immagineCorrente--;
+    } else {
+        immagineCorrente = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        const slide = slides[i];
+
+        if (i == immagineCorrente) {
+            slide.classList.remove("hidden");
+        } else {
+            slide.classList.add("hidden");
+        }
+    }
+
+    // ciclo while
+    let i = 0;
+    while (i < thumbs.length) {
+        const thumb = thumbs[i];
+
+        if (i == immagineCorrente) {
+            thumb.classList.add("activeThumb");
+        } else {
+            thumb.classList.remove("activeThumb");
+        }
+        i++;
+    }
+});
+
+    
+btnNext.addEventListener("click", function () {
+    if (immagineCorrente < slides.length - 1) {
+        immagineCorrente++;
+    } else {
+        immagineCorrente = 0;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        const slide = slides[i];
+
+        if (i == immagineCorrente) {
+            slide.classList.remove("hidden");
+        } else {
+            slide.classList.add("hidden");
+        }
+    }
+
+
+    // ciclo while
+    let i = 0;
+    while (i < thumbs.length) {
+        const thumb = thumbs[i];
+
+        if (i == immagineCorrente) {
+            thumb.classList.add("activeThumb");
+        } else {
+            thumb.classList.remove("activeThumb");
+        }
+        i++;
+    }
+})
+
+
